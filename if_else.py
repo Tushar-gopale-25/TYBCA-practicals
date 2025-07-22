a9bb742680352616ef03d5f7667b9432acf27323
Tushar Gopale <gopaletushar5@gmail.com>
2025-07-22 08:08:38 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the number A: "))
+b=int(input("Enter the number B: "))
+c=int(input("Enter the number C: "))
+if b<a:
+    print(b,"is grather than a")
+elif c>b:
+    print(c,"is grather than b")
+elif a>c:
+    print(a,"is grather than a")
